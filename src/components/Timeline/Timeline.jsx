8e30d64ff89eec17bedae41e99bedb3355c0b50e
RakeshChittala <rakeshchittala8@gmail.com>
2024-06-08 14:13:52 +0530

    second change

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -2,8 +2,40 @@ import "./Timeline.css";
 
 let Timeline = () => {
   return (
-    <div className="timeline-con">
-      <h1 className="maturity-heading">Fd Maturity Timeline</h1>
+    <div className=" timeline-con container">
+      <div className="header">FD Maturity Timeline</div>
+      <div className="timeline">
+        <div className="timeline-header">
+          <div>2024</div>
+          <div>2025</div>
+          <div>2026</div>
+        </div>
+        <div className="timeline-header">
+          <div>J F M A M J J A S O N D</div>
+          <div>J F M A M J J A S O N D</div>
+          <div>J F M A M J J A S O N D</div>
+        </div>
+        <div className="timeline-item">
+          <div className="bar" style={{ width: "120px", marginLeft: "0px" }}>
+            Utkarsh FD 1
+          </div>
+        </div>
+        <div className="timeline-item">
+          <div className="bar" style={{ width: "200px", marginLeft: "20px" }}>
+            Shriram FD Plan 2
+          </div>
+        </div>
+        <div className="timeline-item">
+          <div className="bar" style={{ width: "220px", marginLeft: "40px" }}>
+            Bajaj Finserv FD 1
+          </div>
+        </div>
+        <div className="timeline-item">
+          <div className="bar" style={{ width: "360px", marginLeft: "160px" }}>
+            Mahindra FD Plan 2
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
